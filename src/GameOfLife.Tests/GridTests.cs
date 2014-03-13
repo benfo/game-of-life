09869a41315fc673286596761c37038a430eb513
Ben Fourie <ben.fourie@gmail.com>
2014-03-13 22:15:13 +0200

    Add initial grid and tests

---
+++ src/GameOfLife.Tests/GridTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class GridTests
+    {
+        [Test]
+        [TestCase(5, 5)]
+        [TestCase(10, 5)]
+        [TestCase(5, 10)]
+        public void Constructor_GivenRowsAndColumns_ReturnsGivenRowAndColumnCount(int rows, int columns)
+        {
+            var grid = new Grid(columns, rows);
+
+            Assert.That(grid.Rows, Is.EqualTo(rows));
+            Assert.That(grid.Columns, Is.EqualTo(columns));
+        }
+    }
+}
---
+++ src/GameOfLife/Grid.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife
+{
+    public class Grid
+    {
+        public Grid(int columns, int rows)
+        {
+            Columns = columns;
+            Rows = rows;
+        }
+
+        public int Columns { get; private set; }
+
+        public int Rows { get; private set; }
+    }
+}
