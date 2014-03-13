83cce92e48e4e7f2f2e2ac8478023b85c90fcb4a
Ben Fourie <ben.fourie@gmail.com>
2014-03-13 22:35:53 +0200

    Add methods to add and get a cell

--- src/GameOfLife.Tests/GridTests.cs
+++ src/GameOfLife.Tests/GridTests.cs
@@ -9,12 +9,28 @@ namespace GameOfLife.Tests
         [TestCase(5, 5)]
         [TestCase(10, 5)]
         [TestCase(5, 10)]
-        public void Constructor_GivenRowsAndColumns_ReturnsGivenRowAndColumnCount(int rows, int columns)
+        public void Constructor_GivenRowsAndColumns_ReturnsGivenRowAndColumnCount(int columns, int rows)
         {
             var grid = new Grid(columns, rows);
 
             Assert.That(grid.Rows, Is.EqualTo(rows));
             Assert.That(grid.Columns, Is.EqualTo(columns));
+        }
+
+        [Test]
+        [TestCase(2, 2)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        public void AddCell_GivenACellWithColumnAndRow_AddsCellIntoThatColumn(int column, int row)
+        {
+            var grid = new Grid(4, 4);
+            var cell = new Cell {Column = column, Row = row};
+
+            grid.AddCell(cell);
+
+            Cell addedCell = grid.GetCell(column, row);
+            Assert.That(addedCell.Column, Is.EqualTo(column));
+            Assert.That(addedCell.Row, Is.EqualTo(row));
         }
     }
 }
--- src/GameOfLife/Grid.cs
+++ src/GameOfLife/Grid.cs
@@ -1,15 +1,31 @@
-﻿namespace GameOfLife
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GameOfLife
 {
     public class Grid
     {
+        private readonly Cell[,] _cells;
+
         public Grid(int columns, int rows)
         {
             Columns = columns;
             Rows = rows;
+            _cells = new Cell[columns, rows];
         }
 
         public int Columns { get; private set; }
 
         public int Rows { get; private set; }
+
+        public void AddCell(Cell cell)
+        {
+            _cells[cell.Column - 1, cell.Row - 1] = cell;
+        }
+
+        public Cell GetCell(int column, int row)
+        {
+            return _cells[column - 1, row - 1];
+        }
     }
 }
