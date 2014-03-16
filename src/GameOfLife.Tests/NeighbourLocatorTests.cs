c7a400ab01cb6cd25f7b5ccae8b772d6f046b365
Ben Fourie <ben.fourie@gmail.com>
2014-03-16 13:19:58 +0200

    Implement neighbour checking using offsets

--- src/GameOfLife.Tests/NeighbourLocatorTests.cs
+++ src/GameOfLife.Tests/NeighbourLocatorTests.cs
@@ -3,16 +3,49 @@ using System.Linq;
 
 namespace GameOfLife.Tests
 {
+    [TestFixture]
     public class NeighbourLocatorTests
     {
+        private Grid _grid3X3;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _grid3X3 = new Grid(3, 3);
+        }
+
         [Test]
         public void Find_CellWithNoNeighbours_ReturnsNoNeighbours()
         {
-            var locator = new NeighbourLocator();
-            var cell = new Cell { Column = 0, Row = 0, State = CellState.Alive };
+            var cell = new Cell {Column = 1, Row = 1, State = CellState.Alive};
+            _grid3X3.AddCell(cell);
+            
+            var locator = new NeighbourLocator(_grid3X3);
             var neighbours = locator.Find(cell);
 
             Assert.That(neighbours.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 0)]
+        [TestCase(2, 0)]
+        [TestCase(0, 1)]
+        [TestCase(2, 1)]
+        [TestCase(0, 2)]
+        [TestCase(1, 2)]
+        [TestCase(2, 2)]
+        public void Find_CellWith1Neighbour_Returns1Neighbour(int col, int row)
+        {
+            var cell = new Cell { Column = 1, Row = 1, State = CellState.Alive };
+            _grid3X3.AddCell(cell);
+
+            _grid3X3.AddCell(new Cell { Column = col, Row = row, State = CellState.Alive });
+            
+            var locator = new NeighbourLocator(_grid3X3);
+            var neighbours = locator.Find(cell);
+
+            Assert.That(neighbours.Count(), Is.EqualTo(1));
         }
     }
 }
--- src/GameOfLife/NeighbourLocator.cs
+++ src/GameOfLife/NeighbourLocator.cs
@@ -1,12 +1,57 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GameOfLife
 {
     public class NeighbourLocator
     {
+        private readonly Grid _grid;
+
+        private static readonly Offset[] NeighbourOffsets =
+        {
+            new Offset( -1, -1 ), new Offset( 0, -1 ), new Offset( 1, -1 ),
+            new Offset( -1, 0 ), new Offset( 1, 0 ),
+            new Offset( -1, 1 ), new Offset( 0, 1 ),new Offset( 1, 1 )
+        };
+
+        public NeighbourLocator(Grid grid)
+        {
+            _grid = grid;
+        }
+
         public IEnumerable<Cell> Find(Cell cell)
         {
-            return new Cell[] { };
+            var neighbouringCells = 
+                from offset in NeighbourOffsets
+                select NeighbourAt(cell.Column + offset.ColumnOffset, cell.Row + offset.RowOffset);
+
+            return neighbouringCells
+                .Where(neighbour => neighbour != null);
+        }
+
+        private Cell NeighbourAt(int column, int row)
+        {
+            if (OutOfBounds(column, row)) return null;
+            
+            return _grid.GetCell(column, row);
+        }
+
+        private bool OutOfBounds(int column, int row)
+        {
+            return column < 0 || row < 0 || column >= _grid.Columns || row >= _grid.Rows;
+        }
+
+        private class Offset
+        {
+            public Offset(int columnOffset, int rowOffset)
+            {
+                RowOffset = rowOffset;
+                ColumnOffset = columnOffset;
+            }
+
+            public int ColumnOffset { get; private set; }
+
+            public int RowOffset { get; private set; }
         }
     }
 }
