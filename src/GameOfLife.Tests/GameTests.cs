55e93dee9a9b98fe9cc85459a9852169dac5b12e
Ben Fourie <ben.fourie@gmail.com>
2014-03-16 20:31:02 +0200

    Add Game constructor

--- src/GameOfLife.Tests/GameTests.cs
+++ src/GameOfLife.Tests/GameTests.cs
@@ -6,10 +6,19 @@ namespace GameOfLife.Tests
     public class GameTests
     {
         [Test]
+        public void Constructor_Using3x3_ShouldReturn3x3Grid()
+        {
+            var game = new Game(3, 3);
+
+            Assert.That(game.Grid.Columns, Is.EqualTo(3));
+            Assert.That(game.Grid.Rows, Is.EqualTo(3));
+        }
+
+        [Test]
         public void Tick_GivenANewGame_IncreaseGenerationTo1()
         {
-            var game = new Game();
-            
+            var game = new Game(3, 3);
+
             game.Tick();
 
             Assert.That(game.Generation, Is.EqualTo(1));
--- src/GameOfLife/Game.cs
+++ src/GameOfLife/Game.cs
@@ -1,12 +1,30 @@
-﻿namespace GameOfLife
+﻿using System.Linq;
+
+namespace GameOfLife
 {
     public class Game
     {
+        private Grid _grid;
+
+        public Game(int columns, int rows)
+        {
+            _grid = new Grid(columns, rows);
+        }
         public void Tick()
         {
             Generation += 1;
+
+            foreach (var cell in _grid)
+            {
+                
+            }
         }
 
         public int Generation { get; private set; }
+
+        public Grid Grid
+        {
+            get { return _grid; }
+        }
     }
 }
