4b8d72f0c6a30a10797542cfe5e79a3e60524ab6
Ben Fourie <ben.fourie@gmail.com>
2014-03-16 14:10:15 +0200

    Add NeighbourTestCase helper

--- src/GameOfLife.Tests/NeighbourLocatorTests.cs
+++ src/GameOfLife.Tests/NeighbourLocatorTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
+using NUnit.Framework;
+using System;
 using System.Linq;
 
 namespace GameOfLife.Tests
@@ -15,15 +17,29 @@ namespace GameOfLife.Tests
         }
 
         [Test]
-        public void Find_CellWithNoNeighbours_ReturnsNoNeighbours()
+        [TestCase("cp ," +
+                  "ppp," +
+                  "p  ", 3)]
+        [TestCase("pc ," +
+                  "ppp," +
+                  "p  ", 4)]
+        [TestCase(" c ," +
+                  " pp," +
+                  "p  ", 2)]
+        [TestCase(" p ," +
+                  "pcp," +
+                  "p p", 5)]
+        [TestCase("ppp," +
+                  "pcp," +
+                  "ppp", 8)]
+        public void Find_CellWithNeighbours_ReturnsAllTheNeighbours(string pattern, int neighbourCount)
         {
-            var cell = new Cell {Column = 1, Row = 1, State = CellState.Alive};
-            _grid3X3.AddCell(cell);
-            
-            var locator = new NeighbourLocator(_grid3X3);
-            var neighbours = locator.Find(cell);
+            var testCase = NeighbourTestCase.Create(_grid3X3, pattern);
 
-            Assert.That(neighbours.Count(), Is.EqualTo(0));
+            var locator = new NeighbourLocator(_grid3X3);
+            var neighbours = locator.Find(testCase.Cell).ToArray();
+
+            Assert.That(neighbours.Length, Is.EqualTo(neighbourCount));
         }
 
         [Test]
@@ -40,12 +56,47 @@ namespace GameOfLife.Tests
             var cell = new Cell { Column = 1, Row = 1, State = CellState.Alive };
             _grid3X3.AddCell(cell);
 
-            _grid3X3.AddCell(new Cell { Column = col, Row = row, State = CellState.Alive });
-            
+            var ncell = new Cell { Column = col, Row = row, State = CellState.Alive };
+            _grid3X3.AddCell(ncell);
+
+            var locator = new NeighbourLocator(_grid3X3);
+            var neighbours = locator.Find(cell).ToArray();
+
+            Assert.That(neighbours.Length, Is.EqualTo(1));
+            Assert.That(neighbours[0], Is.EqualTo(ncell));
+        }
+
+        [Test]
+        public void Find_CellWith2Neighbours_Returns2Neighbours()
+        {
+            var cell = new Cell { Column = 1, Row = 1, State = CellState.Alive };
+            _grid3X3.AddCell(cell);
+
+            var ncell1 = new Cell { Column = 0, Row = 0, State = CellState.Alive };
+            var ncell2 = new Cell { Column = 1, Row = 2, State = CellState.Alive };
+            _grid3X3.AddCell(ncell1);
+            _grid3X3.AddCell(ncell2);
+
+            var locator = new NeighbourLocator(_grid3X3);
+            var neighbours = locator.Find(cell).ToArray();
+
+            Assert.That(neighbours.Length, Is.EqualTo(2));
+            Assert.That(neighbours[0], Is.EqualTo(ncell1));
+            Assert.That(neighbours[1], Is.EqualTo(ncell2));
+        }
+
+        [Test]
+        public void Find_CellWithNoNeighbours_ReturnsNoNeighbours()
+        {
+            var cell = new Cell { Column = 1, Row = 1, State = CellState.Alive };
+            _grid3X3.AddCell(cell);
+
             var locator = new NeighbourLocator(_grid3X3);
             var neighbours = locator.Find(cell);
 
-            Assert.That(neighbours.Count(), Is.EqualTo(1));
+            Assert.That(neighbours.Count(), Is.EqualTo(0));
         }
+
+
     }
 }
---
+++ src/GameOfLife.Tests/NeighbourTestCase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace GameOfLife.Tests
+{
+    public class NeighbourTestCase
+    {
+        public NeighbourTestCase()
+        {
+            NeighbouringCells = new List<Cell>();
+        }
+
+        public Cell Cell { get; set; }
+
+        public List<Cell> NeighbouringCells { get; set; }
+    
+        public static NeighbourTestCase Create(Grid grid, string pattern)
+        {
+            var neighbourTestCase = new NeighbourTestCase();
+
+            var rows = pattern.Split(',');
+            for (var rowIndex = 0; rowIndex < rows.Length; rowIndex++)
+            {
+                var row = rows[rowIndex];
+                for (var colIndex = 0; colIndex < row.Length; colIndex++)
+                {
+                    var cellPattern = row[colIndex].ToString(CultureInfo.InvariantCulture);
+
+                    if (string.IsNullOrWhiteSpace(cellPattern))
+                        continue;
+
+                    var cell = new Cell { Column = colIndex, Row = rowIndex };
+                    grid.AddCell(cell);
+                    switch (cellPattern)
+                    {
+                        case "c":
+                            neighbourTestCase.Cell = cell;
+                            break;
+
+                        case "p":
+                            neighbourTestCase.NeighbouringCells.Add(cell);
+                            break;
+                    }
+                }
+            }
+            return neighbourTestCase;
+        }
+
+    }
+}
