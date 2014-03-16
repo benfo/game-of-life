e3c71b6fa3e30f9c0c71fc0c2d83154b87f58485
Ben Fourie <ben.fourie@gmail.com>
2014-03-16 20:03:52 +0200

    Add Game and Tick method

---
+++ src/GameOfLife.Tests/GameTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void Tick_GivenANewGame_IncreaseGenerationTo1()
+        {
+            var game = new Game();
+            
+            game.Tick();
+
+            Assert.That(game.Generation, Is.EqualTo(1));
+        }
+    }
+}
---
+++ src/GameOfLife/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace GameOfLife
+{
+    public class Game
+    {
+        public void Tick()
+        {
+            Generation += 1;
+        }
+
+        public int Generation { get; private set; }
+    }
+}
